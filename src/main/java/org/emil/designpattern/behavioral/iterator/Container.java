c77d2513f1336ce1a6f887fe52572a3bb057372c
Emilio <emilio.gumayagay@gmail.com>
2019-02-06 18:09:54 +0800

    created iterator design pattern

---
+++ src/main/java/org/emil/designpattern/behavioral/iterator/Container.java
@@ -0,0 +1,6 @@
+package org.emil.designpattern.behavioral.iterator;
+
+public interface Container{
+
+    public Iterator getIterator();
+}
---
+++ src/main/java/org/emil/designpattern/behavioral/iterator/Iterator.java
@@ -0,0 +1,8 @@
+package org.emil.designpattern.behavioral.iterator;
+
+public interface Iterator{
+
+    public boolean hasNext();
+    public Object next();
+
+}
